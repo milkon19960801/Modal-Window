6fc16d9e9ca020e93801efb88faaec0f8af8eaf0
milkon <melkon_2010@hotmail.com>
2021-04-08 12:48:57 +0200

    added closing functionality to btn and overlay and refactored the code

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log(btnsOpenModal[i].textContent, 'Button clicked');
-    modal.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
